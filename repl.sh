1279c2c223efef15c1dcfe2baffabd2c14f83440
a1exsh <alex.shulgin@gmail.com>
2025-12-28 19:04:27 +0100

    just run it

--- repl.sh
+++ repl.sh
@@ -5,8 +5,6 @@ export FHOMM_DATA=~/Downloads/homm/data
 ipython3 -i \
          --simple-prompt \
          --InteractiveShellApp.extensions autoreload \
-         --InteractiveShellApp.exec_lines '%autoreload 2'
-
-# from within ipython prompt:
-#
-# >> %run fhomm/main.py
+         --InteractiveShellApp.exec_lines '%autoreload 2' \
+         -- \
+         ./fhomm/main.py
